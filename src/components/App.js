9266e5e9db149fa25256035312bb88a96f26813b
马兴骢 <demonmaxc@gmail.com>
2018-08-21 23:28:31 +0800

    modify structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 import { Button } from "antd";
-import logo from './logo.svg';
-import './App.css';
+import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <Button type="primary">Primary Button</Button>
       </div>
     );
