e24f3ef58796635d1e3c67efd4d0caba2f3db6b0
马兴骢 <demonmaxc@gmail.com>
2018-08-21 23:51:05 +0800

    layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
-import { Button } from "antd";
+import { Row, Col, Button } from "antd";
 import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Button type="primary">Primary Button</Button>
+      <div className="app-container">
+        <Row gutter={30}>
+          <Col xs={24} md={12} lg={12}>
+            <div>Picture</div>
+          </Col>
+          <Col xs={24} md={12} lg={12}>
+            <div>
+              Design
+              <Button type="primary">Download</Button>
+            </div>
+          </Col>
+        </Row>
       </div>
     );
   }
